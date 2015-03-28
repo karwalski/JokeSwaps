037feb9f05f9f33f241afa9e838b3ae0c5629eb3
Matt Watt <matt@karwalski.net>
2015-03-29 08:53:53 +1100

    Added jax

--- jack.php
+++ jack.php
@@ -105,6 +105,7 @@ $conn->close();
 <BR /><BR /><BR />
 <a href="http://mahni.jokeswaps.com" target="_top">Mahni's Joke Swap</a><BR />
 <a href="http://tammi.jokeswaps.com" target="_top">Tammi's Joke Swap</a><BR />
+<a href="http://jax.jokeswaps.com" target="_top">Jax's Joke Swap</a><BR />
 
 <BR />
 About: When the 7 year old twins Tammi and Mahni created their first web pages, they were quickly dissapointed to discover that only they could add jokes to their personal pages, and not to their sisters page. They need a 'JokeSpace' or 'JokeTime' - a social media site allowing them to swap jokes, a few tips from dad and they were coding away creating a SQL database and writting up a php script to save and display the jokes. Ofcours the dinner table discussion that night quickly turned into a discussion of the sites rules and terms and conditions, which will be also written by kids for kids. Visit back regularly to see the site progress as they choose they style the site and hopefully one day open it for parents to register their kids.
