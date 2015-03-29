89f85268cfc6644485dfe7ee451b850f76c81ce6
Matt Watt <matt@karwalski.net>
2015-03-30 10:33:48 +1100

    created parents login page

---
+++ parents.php
@@ -0,0 +1,50 @@
+<?PHP
+
+
+
+?>
+
+
+
+<HTML>
+<HEAD>
+<TITLE>
+JokeSwaps - Parents Console
+</TITLE>
+
+</HEAD>
+<BODY>
+<H1>This is the parents Console</H1>
+
+<FORM METHOD="POST" ACTION="?login=true">
+<STRONG>Login</STRONG><BR />
+<label for="username">Childs username: </label><input type="text" name="username" id="username"><br />
+<label for="password">Parents password: </label><input type="password" name="password" id="password"><br />
+<input type="submit" value="Login"><br />
+</FORM>
+
+
+<FORM METHOD="POST" ACTION="?signup=true">
+<STRONG>Signup</STRONG><BR />
+<label for="username">Childs username: </label><input type="text" name="username" id="username"><br />
+<label for="password">Parents password: </label><input type="password" name="password" id="password"><br />
+<label for="password2">Renter Password: </label><input type="password" name="password2" id="password2"><br />
+
+
+<label for="bio">Childs bio: </label><input type="text" name="bio" id="bio"><br />
+Choose an avatar for your child<br />
+<br /><br /><br /><br />
+
+I declare I am a responsible adult, parent or guardian giving permission for this child to receive a JokeSwaps profile page.<br />
+I declare I have discussed the site rules with the child, and they understand the rules.<br />
+I declare I will be supervising the child when using this website.<br />
+I have read the rules and privacy policy for this website.<br />
+<input type="submit" value="Signup"><br />
+</FORM>
+
+
+
+
+
+</BODY>
+</HTML>
