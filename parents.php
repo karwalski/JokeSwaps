18149321d30845d5aecb804205bcd3f8fd69ffa1
Matt Watt <matt@karwalski.net>
2015-04-05 01:06:24 +1100

    update form script

--- parents.php
+++ parents.php
@@ -49,11 +49,43 @@ if ($conn->query($sql) === TRUE) {
 
 
 
+// Update form submit
+if (isset($_GET['update']) && $_GET['update'] == "true")
+{
+
+$username = $_POST["username"];
+ $username = mysqli_real_escape_string($conn, $username);
+$email = $_POST["email"];
+ $email = mysqli_real_escape_string($conn, $email);
+$bio = $_POST["bio"];
+ $bio = mysqli_real_escape_string($conn, $bio);
+
+$theme = $_POST["theme"];
+$avatar = $_POST["avatar"];
+
+
+$hash = crypt($_POST["password"], (sprintf("$2a$%02d$", 10) . strtr(base64_encode(mcrypt_create_iv(16, MCRYPT_DEV_URANDOM)), '+', '.')));
+
+
+// Save user
+$sql = "UPDATE users SET password='$hash', email='$email', theme='$theme', bio='$bio', avatar='$avatar' WHERE username='$username'";
+
+if ($conn->query($sql) === TRUE) {
+    echo 'Account updated for ' . $username . '!';
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+
+}
+
+
+
+
 // Login
 if (isset($_GET['login']) && $_GET['login'] == "true")
 {
 
-echo 'Checkpoint 1';
 
 $username = $_POST["username"];
  $username = mysqli_real_escape_string($conn, $username);
@@ -65,15 +97,8 @@ $result = $conn->query($sql);
 for ($userInfo = array (); $row = $result->fetch_assoc(); $userInfo[] = $row);
 
 $hash = $userInfo[0]["password"];
-echo 'hash :' . $hash . '<BR />';
-echo 'Checkpoint 2';
-$hash2 = crypt($_POST["password"], $hash);
-
-echo 'hash2 :' . $hash2 . '<BR />';
 
 if ( hash_equals($hash, crypt($_POST["password"], $hash)) ) {
-
-echo 'Checkpoint 3';
 
 $signedIn = 'true';
 
@@ -113,45 +138,24 @@ JokeSwaps - Parents Console
 <BODY>
 <H1>This is the parents Console</H1>
 
-<FORM METHOD="POST" ACTION="?login=true">
-<STRONG>Login</STRONG><BR />
-<label for="username">Childs username: </label><input type="text" name="username" id="username"><br />
-<label for="password">Parents password: </label><input type="password" name="password" id="password"><br />
-<input type="submit" value="Login"><br />
-</FORM>
 
+<?PHP
 
-<FORM METHOD="POST" ACTION="?signup=true">
-<STRONG>Signup</STRONG><BR />
-<label for="username">Childs username: </label><input type="text" name="username" id="username"><br />
-<label for="password">Parents password: </label><input type="password" name="password" id="password"><br />
-<label for="password2">Renter Password: </label><input type="password" name="password2" id="password2"><br />
+if ($signedIn == 'true')
+{
 
+global $userInfo;
 
-<label for="password">Parents email: </label><input type="email" name="email" id="email"><br />
+?>
 
-<label for="theme">Page theme: </label><input type="text" name="theme" id="theme"><br />
-
-<label for="bio">Childs bio: </label><input type="text" name="bio" id="bio"><br />
-Choose an avatar for your child<br />
-<br /><br /><br /><br />
-
-I declare I am a responsible adult, parent or guardian giving permission for this child to receive a JokeSwaps profile page.<br />
-I declare I have discussed the site rules with the child, and they understand the rules.<br />
-I declare I will be supervising the child when using this website.<br />
-I have read the rules and privacy policy for this website.<br />
-<input type="submit" value="Signup"><br />
-</FORM>
-
-<br /><br /><br />
-// If signed in<br />
-Childs username (cannot be changed): <br />
+Childs username (cannot be changed): <?PHP echo $userInfo[0]["username"]; ?><br />
 
 
 Update settings<br />
 <FORM METHOD="POST" ACTION="?update=true">
-<label for="theme">Page theme: </label><input type="text" name="theme" id="theme"><br />
-<label for="bio">Childs bio: </label><input type="text" name="bio" id="bio"><br />
+<label for="email">Parents email: </label><input type="email" name="email" id="email" value="<?PHP echo $userInfo[0]["email"]; ?>"><br />
+<label for="theme">Page theme: </label><input type="text" name="theme" id="theme" value="<?PHP echo $userInfo[0]["theme"]; ?>"><br />
+<label for="bio">Childs bio: </label><input type="text" name="bio" id="bio" value="<?PHP echo $userInfo[0]["bio"]; ?>"><br />
 Choose an avatar for your child<br />
 <br /><br /><br /><br />
 <label for="password">Parents password: </label><input type="password" name="password" id="password"><br />
@@ -178,7 +182,45 @@ if ($result->num_rows > 0) {
 $conn->close();
 
 
+}
+else
+{
 ?>
+
+<FORM METHOD="POST" ACTION="?login=true">
+<STRONG>Login</STRONG><BR />
+<label for="username">Childs username: </label><input type="text" name="username" id="username"><br />
+<label for="password">Parents password: </label><input type="password" name="password" id="password"><br />
+<input type="submit" value="Login"><br />
+</FORM>
+
+
+<FORM METHOD="POST" ACTION="?signup=true">
+<STRONG>Signup</STRONG><BR />
+<label for="username">Childs username: </label><input type="text" name="username" id="username"><br />
+<label for="password">Parents password: </label><input type="password" name="password" id="password"><br />
+<label for="password2">Renter Password: </label><input type="password" name="password2" id="password2"><br />
+
+
+<label for="email">Parents email: </label><input type="email" name="email" id="email"><br />
+
+<label for="theme">Page theme: </label><input type="text" name="theme" id="theme"><br />
+
+<label for="bio">Childs bio: </label><input type="text" name="bio" id="bio"><br />
+Choose an avatar for your child<br />
+<br /><br /><br /><br />
+
+I declare I am a responsible adult, parent or guardian giving permission for this child to receive a JokeSwaps profile page.<br />
+I declare I have discussed the site rules with the child, and they understand the rules.<br />
+I declare I will be supervising the child when using this website.<br />
+I have read the rules and privacy policy for this website.<br />
+<input type="submit" value="Signup"><br />
+</FORM>
+
+<?PHP
+}
+?>
+
 
 </BODY>
 </HTML>
