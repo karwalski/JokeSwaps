3390c6f2ef1b95c077e9c81a2dce73fb8dbe2710
Matt Watt <matt@karwalski.net>
2015-04-04 23:34:48 +1100

    min working password hashing

--- parents.php
+++ parents.php
@@ -29,48 +29,8 @@ $bio = $_POST["bio"];
 $theme = $_POST["theme"];
 $avatar = $_POST["avatar"];
 
-echo 'checkpoint 1';
 
-$password = $_POST["password"];
-
-// A higher "cost" is more secure but consumes more processing power
-$cost = 10;
-
-
-echo mcrypt_get_iv_size(MCRYPT_CAST_256, MCRYPT_MODE_CFB);
-echo mcrypt_create_iv($size, MCRYPT_DEV_URANDOM);
-echo base64_encode(mcrypt_create_iv($size, MCRYPT_DEV_URANDOM));
-
-
-
-
-
-
-
-// Create a random salt
-$salt = strtr(base64_encode(mcrypt_create_iv(16, MCRYPT_DEV_URANDOM)), '+', '.');
-
-echo 'salt = ' . $salt;
-
-// Prefix information about the hash so PHP knows how to verify it later.
-// "$2a$" Means we're using the Blowfish algorithm. The following two digits are the cost parameter.
-$salt = sprintf("$2a$%02d$", $cost) . $salt;
-
-echo 'salt = ' . $salt;
-
-// Hash the password with the salt
-$hash = crypt($password, $salt);
-
-echo 'hash = ' . $hash;
-
-
-
-
-
-
-
-
-// $hash = crypt($_POST["password"], (sprintf("$2a$%02d$", 10) . strtr(base64_encode(mcrypt_create_iv(16, MCRYPT_DEV_URANDOM)), '+', '.')));
+$hash = crypt($_POST["password"], (sprintf("$2a$%02d$", 10) . strtr(base64_encode(mcrypt_create_iv(16, MCRYPT_DEV_URANDOM)), '+', '.')));
 
 echo 'checkpoint 2';
 
