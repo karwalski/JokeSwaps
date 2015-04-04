5d835df17c5173e61e01422bebba4347df073f08
Matt Watt <matt@karwalski.net>
2015-04-04 22:29:31 +1100

    further echo checkpoints

--- parents.php
+++ parents.php
@@ -39,14 +39,18 @@ $cost = 10;
 // Create a random salt
 $salt = strtr(base64_encode(mcrypt_create_iv(16, MCRYPT_DEV_URANDOM)), '+', '.');
 
+echo 'salt = ' . $salt;
+
 // Prefix information about the hash so PHP knows how to verify it later.
 // "$2a$" Means we're using the Blowfish algorithm. The following two digits are the cost parameter.
 $salt = sprintf("$2a$%02d$", $cost) . $salt;
 
+echo 'salt = ' . $salt;
 
 // Hash the password with the salt
 $hash = crypt($password, $salt);
 
+echo 'hash = ' . $hash;
 
 
 
