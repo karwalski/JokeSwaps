fedd373859face392e5ad2bb2dc8ece5eb698738
Matt Watt <matt@karwalski.net>
2015-04-10 20:16:49 +1000

    create emailqueue table

--- script.php
+++ script.php
@@ -36,24 +36,21 @@ if ($conn->connect_error) {
 }
 
 
-$sql = "DROP TABLE MyGuests";
 
-/*
 
 // sql to create table
-$sql = "CREATE TABLE MyGuests (
-id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
-firstname VARCHAR(30) NOT NULL,
-lastname VARCHAR(30) NOT NULL,
-email VARCHAR(50),
-reg_date TIMESTAMP
+$sql = "CREATE TABLE emailQueue (
+EmailID INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+username VARCHAR(255),
+TokenID INT,
+sent TINYINT,
+LastModified TIMESTAMP
 )";
 
-*/
 
 
 if ($conn->query($sql) === TRUE) {
-    echo "Dropped Table MyGuests successfully";
+    echo "Table emailQueue created successfully";
 } else {
     echo "Error creating table: " . $conn->error;
 }
