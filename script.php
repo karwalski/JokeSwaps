8e040623b6fda091d8189dae774de0e92b697633
Matt Watt <matt@karwalski.net>
2015-04-10 20:08:59 +1000

    dropping accidentally created table

--- script.php
+++ script.php
@@ -11,17 +11,13 @@
 
 echo 'This is the temp DB Editor script, take caution in using';
 
-if (!isset($_POST["a"]) && $_POST["a"] == "runDBscript" )
+if (!isset($_POST["a"]))
 {
-echo '
-<form action="#" method="post">
-<input type="hidden" name="a" value="runDBscript"
-<input type="submit" value="Run script">
-</form>';
+e
 
-}
-else
-{
+
+
+
 	
 	
 // Script running
@@ -40,6 +36,11 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+
+$sql = "DROP TABLE MyGuests";
+
+/*
+
 // sql to create table
 $sql = "CREATE TABLE MyGuests (
 id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
@@ -49,14 +50,27 @@ email VARCHAR(50),
 reg_date TIMESTAMP
 )";
 
+*/
+
+
 if ($conn->query($sql) === TRUE) {
-    echo "Table MyGuests created successfully";
+    echo "Dropped Table MyGuests successfully";
 } else {
     echo "Error creating table: " . $conn->error;
 }
 
 $conn->close();
 
+
+
+}
+else
+{
+cho '
+<form action="#" method="post">
+<input type="hidden" name="a" value="runDBscript"
+<input type="submit" value="Run script">
+</form>';
 
 }
 
