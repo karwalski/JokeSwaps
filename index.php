3e2d8979062a8caf919f600d5f2f70c09fee9aa6
Matt Watt <matt@karwalski.net>
2015-04-13 00:00:20 +1000

    edit about footer

--- index.php
+++ index.php
@@ -443,7 +443,7 @@ if ($result->num_rows > 0) {
 <a href="http://felix.jokeswaps.com" target="_top">Felix's Joke Swap</a><BR />
 
 <BR />
-About: When the 7 year old twins Tammi and <?PHP echo $user; ?> created their first web pages, they were quickly dissapointed to discover that only they could add jokes to their personal pages, and not to their sisters page. They need a 'JokeSpace' or 'JokeTime' - a social media site allowing them to swap jokes, a few tips from dad and they were coding away creating a SQL database and writting up a php script to save and display the jokes. Ofcours the dinner table discussion that night quickly turned into a discussion of the sites rules and terms and conditions, which will be also written by kids for kids. Visit back regularly to see the site progress as they choose they style the site and hopefully one day open it for parents to register their kids.
+About: When the 7 year old twins Tammi and Mahni created their first web pages, they were quickly dissapointed to discover that only they could add jokes to their personal pages, and not to their sisters page. They needed a 'JokeSpace' or 'JokeTime' - a social media site allowing them to swap jokes, a few tips from dad and they were coding away creating a SQL database and writting up a php script to save and display the jokes. Ofcourse the dinner table discussion that night quickly turned into a discussion of the sites rules and terms and conditions, which will be also written by kids for kids. Visit back regularly to see the site progress as they choose their style for the site and hopefully one day open it for parents to register their kids.
 
 
 
