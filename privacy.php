b32112fc7d2b887358327244dfdaf06b3ca71134
Matt Watt <matt@karwalski.net>
2015-04-23 00:47:31 +1000

    t&c + privacy pages

---
+++ privacy.php
@@ -0,0 +1,164 @@
+<?PHP
+
+?>
+
+<HTML>
+<HEAD>
+
+<TITLE>
+JokeSwaps - Privacy Policy
+</TITLE>
+
+    <meta charset="utf-8" />
+    <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <link rel="stylesheet" href="css/960_16_col.css" />
+    <link rel="stylesheet" href="css/reset.css" />
+    <link rel="stylesheet" href="css/text.css" />
+    <link rel="stylesheet" href="css/mainstyle.css" />
+	
+
+	</head>
+
+
+	<body>
+	    <div id="wrapper" class="container_16">
+	        <!--Start of body wrraper-->
+	        <header class="container_16">
+	            <!--Start of Header-->
+	            <div id="logo">
+	                <img src="Images/logo.png" class="logo" />
+	            </div>
+	            <div id="hdr_img">
+
+	                <img src="Images/header-img.png" />
+
+	            </div>
+
+	        </header>
+	        <!--End of Header-->
+
+	
+PRIVACY POLICY
+
+For... JokeSwaps
+
+
+NOTICE:  Your privacy is critically important to us. At JokeSwaps, we have a few fundamental principles.
+
+The purpose of this Privacy Policy is to spell out how we deal with any personal data that we obtain from the user of our website or that you provide to us while visiting http://www. jokeswaps..com.
+
+ We use your information only as set out in this Privacy Policy. Naturally, if you are not pleased with this Website Privacy Policy you should not use this Website.
+
+ By using or accessing our website, you expressly consent to our collection, storage, use and disclosure of your personal information as described in this Privacy Policy.
+
+At JokeSwaps, we reserve the right to make changes to this Privacy Policy at any time, and it will be posted on this page after the information collected is applied. 
+This online policy applies only to information collected through this website only, and not to information provided offline. If the user links to outside websites, please review their posted Privacy Policies. 
+What We Collect... We may collect the following information:
+Name 
+Contact information including email address
+Demographic information such as postcode, preferences and interests
+Contact Number
+Other information relevant to customer surveys and/or offers
+Information Collected
+We collect personal information when registering online, placing an order, or communicating via email. We may ask for your name, mailing address, phone number, email address, and credit card information when registering and/or placing an order, however you may visit our website anonymously.
+
+ The information you provide is beneficial to providing better service in responding to your requests, processing your transactions, and to provide our visitors with the latest information pertaining to our equipment, parts and repairs. All newsletters from our site, allow visitors to unsubscribe if they wish to do so. 
+
+Our visitor’s information are well saved with us
+All the personal details of our users obtained are stored for contact purposes online. But those details will not be divulged to anyone else.  
+When do we collect information relating to http://www. jokeswaps..com?
+
+We collect information from you when you register on our site, make a request, respond to a survey, fill out a form or enter information on our website.
+ How do we use or process your information? 
+
+We may use the information we collect from you when you register, make a request, sign up for our newsletter, respond to a survey,  surf the website, or use certain other site features in the following ways:
+To provide outdoor information
+To process our users’ requests
+To update all the users of our website
+To enable us at JokeSwap carry out our mission and goals effectively.
+To keep a record of your correspondence with us
+To have details of your transaction carry out on our website
+What we do With the Information We Gather We require this information to understand your needs and provide you with a better service, and in particular for the following reasons:
+Internal record keeping.
+We may use the information to improve our products and services.
+We may periodically send promotional emails about new products, special offers or other information which we think you may find interesting using the email address which you have provided.
+From time to time, we may also use your information to contact you for market research purposes. We may contact you by email, phone, fax or mail. We may use the information to customise the website according to your interests
+Security...Protecting our Visitors
+We are committed to ensuring that your information is secure. In order to prevent unauthorised access or disclosure, we have put in place suitable physical, electronic and managerial procedures to safeguard and secure the information we collect online.
+
+We implement a variety of security measures to maintain the safety of our visitor’s personal information. Your information is contained behind secure networks and is only accessible by a limited number of authorized employees, contractors and agents.
+
+ All personal and credit information provided is transmitted through Secure Socket Layer (SSL) technology and then encrypted into our database only to be accessed by our authorized agents.
+
+Fraud Prevention and Investigations
+We may share information with government agencies or other companies assisting us in fraud prevention or investigations when: 
+Required or permitted by law 
+Protecting or preventing against an unauthorized transaction or potential fraud
+Investigating fraud which has already taken place
+Site Cookies and Tracking
+This site uses cookies that enable the site or service provider’s systems to recognize your browser and operating system to remember certain information, such as items added to your cart. We also use cookies to help us gather data about website traffic and provide us an understanding how visitors use our site so that we can offer better site experiences and tools to improve customer service.
+  
+Personal information cannot be collected through cookies or other tracking files, however if you previously provided personal information, cookies may be tied to this information. 
+
+Your computer can warn you if cookie is being sent, or you can disable cookies entirely through your browser settings, however doing so will disable certain accessible features which make your site experience more efficient. 
+
+Cookie and tracking information may be shared with third party company links; however no personally identifiable information can be disclosed or shared with these companies.
+
+Third Party Links
+To provide our visitors with an extra service for visiting our site, we may include third party links. These linked sites are not affiliated with our privacy policy; we therefore are not liable or responsible for the content and activities these links provide.
+ 
+We welcome any feedback about these site links including if the provided link is no longer available.
+
+ COPPA (Children Online Privacy Protection Act)
+
+JokeSwap does not intentionally obtain personal identifiable information from children under the age of thirteen (13) without verifiable parental consent. However, if it is determined that such information has been inadvertently obtained on anyone fewer than thirteen (13), we shall immediately take the necessary steps to ensure that such information is deleted from our system’s database.
+ 
+Anyone fewer than thirteen (13) must seek and obtain parent or guardian permission to use this website
+. Privacy Policy Changes to JokeSwap
+Although most changes are likely to be minor, JokeSwap may change its privacy Policy from time to time, and in JokeSwaps sole discretion..JokeSwap encourage visitors to frequently check this page for any changes to its Privacy Policy. However, your continued use of this site after any change in this Privacy Policy will constitute your acceptance of such change.
+.Acceptance of Terms 
+Through the use of  http://www. jokeswaps.com, you are hereby accepting the terms and conditions stipulated within the aforementioned Privacy Policy Agreements. If you are not in agreement with our terms and conditions, then you should refrain from further use of our sites. In addition, your continued use of our website following the posting of any updates or changes to our terms and conditions shall mean that you are in agreement and acceptance of such changes. 
+  For Questions or Comments, Contact Us:
+If you have any questions, concerns, comments or complaints regarding this Privacy Policy and/or our collection or use of personal information, or if you wish us to stop processing your personal information for any particular purpose or purposes, then please contact: Matt Watt, JokeSwaps, admin@jokeswaps.com, Contact number: + 0409463786.
+
+
+
+
+
+	    </div>
+	    <!--End of Body wrapper-->
+	    <footer id="footerBg">
+	        <form id="contactForm" class="container_16">
+
+	            <div class="prefix_8 grid_3 alpha label">
+
+	                <label for="name" class="grid_3">Name - </label>
+
+	                <label for="email" class="grid_3">E-mail - </label>
+
+	                <label for="Message" class="grid_3">Message - </label>
+
+	            </div>
+
+
+	            <div class="grid_5 omega inputs">
+
+	                <input type="text" name="name" class="grid_5" id="contactName" />
+
+	                <input type="email" name="email" class="grid_5" id="contactEmail" />
+
+	                <textarea name="message" id="contactMessage" cols="30" rows="8"></textarea>
+	                <a href="#"><h1 class="prefix_3 grid_2" id="contactSubmit">GO!</h1></a>
+
+	            </div>
+
+
+	        </form>
+
+
+	    </footer>
+	</body>
+
+	</html>
---
+++ tac.php
@@ -0,0 +1,188 @@
+<?PHP
+
+?>
+
+<HTML>
+<HEAD>
+
+<TITLE>
+JokeSwaps - Privacy Policy
+</TITLE>
+
+    <meta charset="utf-8" />
+    <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <link rel="stylesheet" href="css/960_16_col.css" />
+    <link rel="stylesheet" href="css/reset.css" />
+    <link rel="stylesheet" href="css/text.css" />
+    <link rel="stylesheet" href="css/mainstyle.css" />
+	
+
+	</head>
+
+
+	<body>
+	    <div id="wrapper" class="container_16">
+	        <!--Start of body wrraper-->
+	        <header class="container_16">
+	            <!--Start of Header-->
+	            <div id="logo">
+	                <img src="Images/logo.png" class="logo" />
+	            </div>
+	            <div id="hdr_img">
+
+	                <img src="Images/header-img.png" />
+
+	            </div>
+
+	        </header>
+	        <!--End of Header-->
+
+	
+TERMS AND CONDITIONS
+For... JokeSwaps
+ Please read these Terms and Conditions carefully before using http://www. Jokeswaps.com,  the website (the "Service") operated by JokeSwaps ("us", "we", or "our").
+Your access to and use of the Service is conditioned on your acceptance of and compliance with these Terms. These Terms apply to all visitors, users and others who access or use the Service.
+By accessing or using any part of the WEBSITE, you agree to be bound by the Terms and Conditions of these Agreements. If you disagree with any part of the terms of the agreements, then you may not access the website or use the service. 
+Provision of Service 
+This sets out the basic obligation of JokeSwaps to render services which are part of the agreement. If any changes need to be done by client or the user of our website then, the wording will need to be changed slightly or else this point can be dealt with in the Schedule. 
+ JokeSwaps runs and manage an incredibly, efficient and effective social media website for young kids mainly between 5-13 years old.
+At JokeSwaps, we are dedicated to have a flag/report feature for inappropriate content, and this should be done before contacting admin.
+ Meanwhile, all content provided on http://www. Jokeswaps.com is for informational purposes only.  JokeSwaps makes no representations as to the accuracy or completeness of any information on this site or found by following any link on this site.
+The http://www.Jokeswaps.com will not be liable for any errors or omissions in this information nor for the availability of this information. The JokeSwaps will not be liable for any losses, injuries, or damages from the display or use of this information.
+ Site Access
+It is not necessary to register with us in order to use most parts of this Website. [However, particular areas of this Website will only be accessible only if you have registered.] 
+Can users create an account on our website?
+Yes, users can create an account but it is only the Parents that Must sign up for their kids.
+Reason users must create an account with us:
+
+ To seek or catch fun from the services we provide on our website, parents must create accounts for their kids and complete all information required by us
+
+ We collect your details in order to:
+
+Send information; respond to inquiries, and/or other requests or question
+
+Process request and to send information and updates pertaining to request. 
+
+ How safe is our user’s information?
+
+The information supplied by those surfing our website are very much safe and secured on our database.
+
+ Use of Site
+This Website may be used for your own private purposes and in accordance with these terms of use. 
+ Site Uptime
+All reasonable measures are taken by us to ensure that this Website is operational every day. However, occasionally technical issues may result in some downtime and accordingly we will not be liable if this website is unavailable at any time.
+Where possible we always try to give advance warning of maintenance issues that may result in Website down time but we shall not be obliged to provide such notice.  
+
+Visitor Provided Materials
+Any material that a visitor to this Website sends or posts to this Website shall be considered non-proprietary and non confidential. We shall be entitled to copy, disclose, distribute or use for such other purpose as we deem appropriate all material provided to us, with the exception of personal information, the use of which is covered under our Privacy Policy.  
+When using this website you shall not post or send to or from this Website any material:
+For which you have not obtained all necessary consents;
+That is discriminatory, obscene, pornographic, defamatory, liable to incite racial hatred, in breach of confidentiality or privacy, which may cause annoyance or inconvenience to others, which encourages or constitutes conduct that would be deemed a criminal offence, give rise to a civil liability, or otherwise is contrary to the law in the United Kingdom; 
+Information which is harmful in nature
+ Better still, at JokeSwaps :
+Jokes only
+No comments or messages
+Don't be rude or offensive
+Don't share personal information
+Don't be a bully
+Only share your secret word with friends
+ Important Information for the kids:
+Think about what you are doing and how you are behaving online. 
+Tell your Mum or Dad if you see something online that upsets you. 
+Tell your parent if someone makes you feel unhappy. 
+You can also talk to a trusted adult, like a teacher: they can help. 
+Hide your password. 
+Only ever share your password with your parents - never with your friends. Someone else could go online pretending to be you and do something that could get you into trouble. -Interesting websites can be fun.
+ Check with Mum or Dad if a site is okay to surf 
+Never give your real name, address or phone number to anyone, you don't know in the real world.
+ Use a nickname in chat rooms or when you play games on the computer.
+Disclaimer Statements
+ JokeSwaps does not represent or endorse the accuracy or reliability of any information's, content or advertisements contained on, distributed through, or linked, downloaded or accessed from any of the services contained on this website, nor the quality of any products, information's or any other material displayed, purchased, or obtained by you as a result of an advertisement or any other information's or offer in or in connection with the services herein.
+You hereby acknowledge that any reliance upon any materials shall be at your sole risk.  JokeSwaps reserves the right, in its sole discretion and without any obligation, to make improvements to, or correct any error or omissions in any portion of the service or the materials.
+
+JokeSwaps include the “Disclaimer” and Limitation of Liability” disclosures
+The services and the materials are proved by JokeSwaps on an "AS IS" basis, and JokeSwaps expressly disclaims any and all warranties, express or implied, without limitation warranties of merchantability and fitness for a particular purpose, with respect to the service or any materials and products in no event shall to JokeSwaps.
+Be reliable for any direct, indirect, incidental, punitive, or consequential damages of any kind of what’s over with respect to the service, the materials and products.
+JokeSwaps respects the rights (including the intellectual property rights) of others and we ask our users to do the same.  JokeSwaps may in appropriate circumstances and in its sole discretions, terminate the accounts of users that infringe or otherwise violate such rights of others.
+ LINKS TO AND FROM OTHER WEBSITES
+
+Throughout this Website you may find links to third party websites. The provision of a link to such a website does not mean that JokeSwaps endorse that website.  If you visit any website via a link on this Website you do so at your own risk.
+Any party wishing to link to this website is entitled to do so provided that the conditions below are observed:
+you do not seek to imply that we are endorsing the services or products of another party unless this has been agreed with us in writing;
+You do not misrepresent your relationship with this website; and 
+The website from which you link to this Website does not contain offensive or otherwise controversial content or, content that infringes any intellectual property rights or other rights of a third party.
+By linking to this Website in breach of our terms, you shall indemnify us for any loss or damage suffered to this Website as a result of such linking. 
+
+Consequential Loss 
+Under no circumstances will JokeSwaps s be responsible or liable for financial or other loss or damage caused by the failure or use or misuse of our website]
+If maintenance contract is signed then JokeSwaps will ensure that data on their site is regularly backed up manually and that a contingency plan is in place to minimize possible losses as a result of software failure.
+ 
+Unfortunately malicious software, spy-ware, viruses and website hacking are common threats and   JokeSwaps, cannot be held responsible for problems that develop on completed sites as a result of such illegal activity. 
+
+
+  Supply of Materials 
+The users of our website are to supply all information required for JokeSwaps to complete the demand accordance with the agreed specification. 
+Such information may include, but are not limited to, photographs, written-copy and other printed materials. 
+
+Termination
+We may terminate or suspend access to our Service immediately, without prior notice or liability, for any reason whatsoever, including without limitation if you breach the Terms.
+All provisions of the Terms which by their nature should survive termination shall survive termination, including, without limitation, ownership provisions, warranty disclaimers, indemnity and limitations of liability.
+Governing Law
+These Terms shall be governed and construed in accordance with the Laws of Australia, without regard to its conflict of law provisions.
+Our failure to enforce any right or provision of these Terms will not be considered a waiver of those rights. If any provision of these Terms is held to be invalid or unenforceable by a court, the remaining provisions of these Terms will remain in effect. These Terms constitute the entire agreement between us regarding our Service, and supersede and replace any prior agreements we might have between us regarding the Service.
+Terms and Conditions Changes
+These terms & conditions may change from time to time. The Client will be informed of revisions as and when they are issued. 
+We reserve the right, at our sole discretion, to modify or replace these Terms at any time. If a revision is material we will try to provide at least 30 days notice prior to any new terms taking effect as indicated above. What constitutes a material change will be determined at our sole discretion.
+By continuing to access or use our Service after those revisions become effective, you agree to be bound by the revised terms. If you do not agree to the new terms, please stop using the Service. 
+However, for any material changes to the Terms and Conditions, we at JokeSwaps must notify users in advance. In view of this, we provide 30 days notice (recommended) before the new Terms and Conditions become effective.
+
+More so, we reserve the right to be able to terminate access to certain users, if these users abuse our website.
+
+This is to inform our users that we reserve the right to change our module of operation at any time
+. 
+NOTICE:  We still want to make it clear here that the content, logo, video or, visual design etc and trademarks are exclusive property of JokeSwaps
+
+For Questions or Comments, Contact Us
+If you have any questions, concerns, comments or complaints regarding this Terms and Conditions and/or our collection or use of personal information, or if you wish us to stop processing your personal information for any particular purpose or purposes, then please contact: Matt Watt, JokeSwaps, admin@jokeswaps.com, Contact number: + 0409463786.
+
+
+
+
+
+	    </div>
+	    <!--End of Body wrapper-->
+	    <footer id="footerBg">
+	        <form id="contactForm" class="container_16">
+
+	            <div class="prefix_8 grid_3 alpha label">
+
+	                <label for="name" class="grid_3">Name - </label>
+
+	                <label for="email" class="grid_3">E-mail - </label>
+
+	                <label for="Message" class="grid_3">Message - </label>
+
+	            </div>
+
+
+	            <div class="grid_5 omega inputs">
+
+	                <input type="text" name="name" class="grid_5" id="contactName" />
+
+	                <input type="email" name="email" class="grid_5" id="contactEmail" />
+
+	                <textarea name="message" id="contactMessage" cols="30" rows="8"></textarea>
+	                <a href="#"><h1 class="prefix_3 grid_2" id="contactSubmit">GO!</h1></a>
+
+	            </div>
+
+
+	        </form>
+
+
+	    </footer>
+	</body>
+
+	</html>
