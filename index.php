a8fbf5c3463a89eb5496055dfb7b7acd4dba14a4
Matt Watt <matt@karwalski.net>
2015-04-22 19:09:42 +1000

    add new theme

--- index.php
+++ index.php
@@ -139,6 +139,18 @@ $funcaptcha = new FUNCAPTCHA();
 <TITLE>
 JokeSwaps - <?PHP echo $user; ?>
 </TITLE>
+
+    <meta charset="utf-8" />
+    <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <link rel="stylesheet" href="css/960_16_col.css" />
+    <link rel="stylesheet" href="css/reset.css" />
+    <link rel="stylesheet" href="css/text.css" />
+    <link rel="stylesheet" href="css/mainstyle.css" />
+
+
+
 <script>
 
 
@@ -325,7 +337,13 @@ echo 'black';
 
 </HEAD>
 <BODY>
-<IMG SRC="js_<?PHP
+	
+    <div id="wrapper" class="container_16">
+        <!--Start of body wrraper-->
+        <header class="container_16">
+            <!--Start of Header-->
+            <div id="logo">
+                <img src="Images/js_<?PHP
 
 if ($userInfo[0]["theme"] == "darkmagenta") {
 echo 'pink';
@@ -343,115 +361,252 @@ else {
 echo 'blue';
 }
 
-?>.jpg" width="400px"><BR />
-<H1><?PHP echo $user; ?>'s Joke Swap </H1>
+?>.png" class="logo" />
+            </div>
+            <div id="hdr_img">
 
-<STRONG><?PHP echo $userInfo[0]["bio"]; ?></STRONG><BR /><BR />
+                <img src="Images/header-img.png" />
+
+            </div>
+
+        </header>
+        <!--End of Header-->
+	
+	
+    <section id="userBio" class="container_16">
+        <!--Start of User Bio-->
+        <div id="userInfo" class=" grid_4 alpha">
+            <!--Start of User Info-->
+            <div id="userBg" class="grid_4">
+                <img class="img2" src="Images/avatar.png" />
+            </div>
+            <div id="userName" class="grid_4">
+                <p>Name - <span id="name"><?PHP echo $user; ?></span>
+                </p>
+            </div>
+        </div>
+        <!--End of User Info-->
+
+        <div id="userScore" class="grid_6">
+            <!--Start of User Score-->
+            <div id="scoreTitle" class="grid_1 suffix_5">
+                <p>Scorecard</p>
+            </div>
+            <div id="scoreProgress" class="grid_6">
+                <progress id="progress" value="0.6" max="100%" class="grid_4 omega"> </progress>
+                <h4 class="grid_1">60%</h4>
+
+            </div>
+            <div id="bio" class="grid_6">
+                <h4>About <?PHP echo $user; ?>...</h4>
+                <p class="grid_4"><?PHP echo $userInfo[0]["bio"]; ?></p>
+            </div>
+        </div>
+        <!--End of User Score-->
+	
+	
+
+        <div id="userFriend" class="grid_5 omega">
+            <!--Start of Friend-->
+            <div id="friendTitle">
+                <h3>FRIEND LIST</h3>
+            </div>
+            <form>
+                <input id="friendSearch" type="search" name="search" placeholder="Search" />
+
+            </form>
+            <!--End of Search form-->
+
+            <div id="friendGrid" class="grid_5">
+                <!--Startof Friend Grid-->
+                <div class="friend-bio grid_2">
+                    <img src="Images/avatar.png" height="35px" width="35px" />
+                    <h6 id="friendName"><a href="http://mahni.jokeswaps.com" target="_top">Mahni</a></h6>
+                </div>
+
+                <div class="friend-bio grid_2">
+                    <img src="Images/avatar.png" height="35px" width="35px" />
+
+                    <h6 id="friendName"><a href="http://tammi.jokeswaps.com" target="_top">Tammi</a></h6>
+                </div>
+
+                <div class="friend-bio grid_2">
+                    <img src="Images/avatar.png" height="35px" width="35px" />
+                    <h6 id="friendName"><a href="http://jack.jokeswaps.com" target="_top">Jack</a></h6>
+                </div>
+
+                <div class="friend-bio grid_2">
+                    <img src="Images/avatar.png" height="35px" width="35px" />
+                    <h6 id="friendName">Peter Parker</h6>
+                </div>
+            </div>
+            <!--End of Friend Grid-->
+        </div>
+        <!--End of User Friend-->
+
+    </section>
+    <!--End of User Bio-->
+
+	
+	
+	
+	    <!-- Need to add in preset joke menu and associated prefill
+	<?PHP
+	// Print jokes on users page.. need to check suitable column
+	$sql = "SELECT * FROM PresetJokes" ;
+	$result = $conn->query($sql);
+	$count = $result->num_rows;
+
+	$rand1 = rand(1, $count);
+	$rand2 = rand(1, $count);
+	$rand3 = rand(1, $count);
+	$rand4 = rand(1, $count);
+	$rand5 = rand(1, $count);
+	?>
 
 
 
-<?PHP
-// Print jokes on users page.. need to check suitable column
-$sql = "SELECT * FROM PresetJokes" ;
-$result = $conn->query($sql);
-$count = $result->num_rows;
+	<SCRIPT>
+	function loadPreset() {
 
-$rand1 = rand(1, $count);
-$rand2 = rand(1, $count);
-$rand3 = rand(1, $count);
-$rand4 = rand(1, $count);
-$rand5 = rand(1, $count);
-?>
+	var selectedJoke = document.getElementById("preset").value;
 
+	var jokes = [];
+	var answer = [];
 
+	<?PHP
+	$sql = "SELECT * FROM PresetJokes WHERE PresetID IN ('$rand1','$rand2','$rand3','$rand4','$rand5')" ;
+	$result = $conn->query($sql);
 
-<SCRIPT>
-function loadPreset() {
+	while($row = $result->fetch_assoc()) {
+	echo 'jokes["id' . $row['PresetID'] . '"] = "' . $row['joke'] . '";';
+	echo 'answer["id' . $row['PresetID'] . '"] = "' . $row['answer'] . '";';
+	}
+	?>
 
-var selectedJoke = document.getElementById("preset").value;
+	document.getElementById("joke").value = jokes["id" + selectedJoke];
+	document.getElementById("answer").value = answer["id" + selectedJoke];
+	}
+	</SCRIPT>
+	Preset Joke Selection: <SELECT id="preset" name="preset" onChange="loadPreset();">
+	<OPTION value=""></OPTION>
 
-var jokes = [];
-var answer = [];
-
-<?PHP
-$sql = "SELECT * FROM PresetJokes WHERE PresetID IN ('$rand1','$rand2','$rand3','$rand4','$rand5')" ;
-$result = $conn->query($sql);
-
-while($row = $result->fetch_assoc()) {
-echo 'jokes["id' . $row['PresetID'] . '"] = "' . $row['joke'] . '";';
-echo 'answer["id' . $row['PresetID'] . '"] = "' . $row['answer'] . '";';
-}
-?>
-
-document.getElementById("joke").value = jokes["id" + selectedJoke];
-document.getElementById("answer").value = answer["id" + selectedJoke];
-}
-</SCRIPT>
-Preset Joke Selection: <SELECT id="preset" name="preset" onChange="loadPreset();">
-<OPTION value=""></OPTION>
-
-<?PHP
+	<?PHP
 
 
-$sql = "SELECT * FROM PresetJokes WHERE PresetID IN ('$rand1','$rand2','$rand3','$rand4','$rand5')" ;
-$result = $conn->query($sql);
+	$sql = "SELECT * FROM PresetJokes WHERE PresetID IN ('$rand1','$rand2','$rand3','$rand4','$rand5')" ;
+	$result = $conn->query($sql);
 
-while($row = $result->fetch_assoc()) {
-echo '<OPTION value="' . $row['PresetID'] . '">' . $row['joke'] . '</OPTION>';
+	while($row = $result->fetch_assoc()) {
+	echo '<OPTION value="' . $row['PresetID'] . '">' . $row['joke'] . '</OPTION>';
 
-}
-?>
-</SELECT>
-<BR /><BR />
+	}
+	?>
+	</SELECT>
+	<BR /><BR />
+	-->
+	
 
+    <!-- Need to add back in the knock knock switch and associated labels/autocomplete
+	<label for="name">Is your joke a Knock Knock joke? </label><input type="checkbox" name="knock" id="knock" value="knock" onChange="jokeType()"><BR />
+	<span id="line1"></span><BR />
+-->
 
+	
+	
+	
+    <section id="questionSubmit" class="container_16">
+        <!--Start of question section-->
+        <div class="container_16" id="questionForm">
 
-<STRONG>Write a joke on <?PHP echo $user; ?>'s page</STRONG>
-<FORM METHOD="POST" ACTION="<?php echo $_SERVER['REQUEST_URI']?>">
-<input type="hidden" name="new" id="new" value="joke">
-<label for="name">Your Name: </label><input type="text" name="name" id="name" required><BR />
-<label for="secret" title="A secret word is a password shared by the page owner only to people they know">The secret word: </label><input type="text" name="secret" id="secret" required><BR />
+            <div class="grid_6 alpha" id="logIn">
+                <div class="grid_5" id="titleBg">
+                </div>
+                <h3 class="grid_2" id="logInTitle">Fill Form</h3>
+                <FORM METHOD="POST" ACTION="<?php echo $_SERVER['REQUEST_URI']?>" class="grid_6">
+                    <input type="hidden" name="new" id="new" value="joke">
+					<input class="grid_5" type="text" name="name" id="name" placeholder="Name..." />
+                    <input class="grid_5" type="text" name="secret" id="secret" placeholder="Secret Word..." />
+                    <textarea class="grid_5" name="question" placeholder="Question..." contenteditable="true" rows="3" cols="20" name="joke" id="joke" required onChange="jokeChange();" onInput="jokeInput();"></textarea>
+                    <textarea class="grid_5" name="answer" placeholder="Answer..." contenteditable="true" rows="3" cols="20" name="answer" id="answer" required></textarea>
 
-<label for="name">Is your joke a Knock Knock joke? </label><input type="checkbox" name="knock" id="knock" value="knock" onChange="jokeType()"><BR />
-<span id="line1"></span><BR />
+                
 
-<label for="joke"><span id="line2">Joke Question: </span></label><textarea rows="3" cols="50" name="joke" id="joke" required onChange="jokeChange();" onInput="jokeInput();"></textarea><BR />
-<label for="answer"><span id="line3">Answer: </span></label><textarea rows="3" cols="50" name="answer" id="answer" required></textarea><BR />
+            </div>
+            <div class="grid_5" id="captcha">
+                <div class="grid_5" id="captchaBg">
+                    <div class="grid_4 center" id="captchaBtn">
+						<?php 
+						echo $funcap; 
+						?>
 
-<?php 
-echo $funcap; 
-?>
+                    </div>
+                </div>
 
+            </div>
 
-<input type="submit" value="Submit">
-</FORM>
-<BR /><BR />
-<STRONG><?PHP echo $user; ?>'s Joke Feed</STRONG><BR />
+            <div class="grid_4" id="submit">
+
+                <a href="#">
+                    <div class="grid_3 omega " id="submitBtn">
+						<button type="submit" value="Submit">
+                        <img id="submitCheck" src="Images/check.png" />
+					</button>
+                    </div>
+                </a>
+                <h2 class="grid_5" id="submitTitle">SUBMIT </h2>
+				</form>
+
+            </div>
+
+        </div>
+
+        <div class="container_16" id="questionLine">
+
+            <div class="arrow grid_1 alpha" id="upArrow">
+                <a href="#" class="arrow-link"><h1>&#x3008;</h1></a>
+            </div>
+			
+
 <?PHP
 
 // Print jokes on users page
 $sql = "SELECT * FROM jokes WHERE forUser = '$user' ORDER BY id DESC" ;
 $result = $conn->query($sql);
+$count = 1;
 
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo '<strong>' . $row["fromName"] . ':</strong> ';
+        echo '         <div class=" grid_3 section' . $count . ' inline"';
+		
+		if ($count > 4)
+			{ echo 'style="visibility:hidden;"'}
+		echo '>
+                <div class="box1 grid_3"></div>
+                <div class="box2  grid_3">
+                    <p class="grid_3 question"><strong>' . $row["fromName"] . ':</strong> ';
 		if ($row["type"] == "knock")
 		{
 			echo 'Knock knock<BR />Who\'s there?<BR />';
 		}
 		
-		echo $row["joke"] . '<BR /><button onClick="showAnswer(' . $row["id"] . ');">';
+		echo $row["joke"];
 		if ($row["type"] == "knock")
 		{
-		echo $row["joke"] . ' who?';
+		echo '<BR />' . $row["joke"] . ' who?';
 		}
-		else
-		{
-		echo 'Here\'s the answer';
-		}
-		echo '</button><BR/><div id="Answer' . $row["id"] . '" style="visibility:hidden;">';
+		
+		echo '</p>
+			                </div>
+			                    <button class="grid_3 btn" id="section' . $count . 'Btn" onClick="showAnswer(' . $row["id"] . ');">
+			                        <h2 class="btn.text">
+									SHOW ANSWER</h2>
+									                    </button>
+									          
+			
+		
+<div id="Answer' . $row["id"] . '" style="visibility:hidden;">';
         echo $row["answer"] . '<BR /></div>';
 		echo '<button onClick="funnyButton(' . $row["id"] . ');" id="funnyButton_' . $row["id"] . '">This is funny</button>';
 		echo '<button onClick="showFlagSelect(' . $row["id"] . ');" id="FlagButton_' . $row["id"] . '">Report joke</button>';
@@ -466,7 +621,8 @@ if ($result->num_rows > 0) {
 <option value="6">Spam written by a robot</option>
 </select></div>';
 		
-		echo '<BR /><BR />';
+		echo '  </div>';
+		$count = $count++;
     }
 } else {
     echo "No jokes yet, send " . $user . " a joke now";
@@ -475,16 +631,57 @@ if ($result->num_rows > 0) {
 
 
 ?>
+            <div class="arrow grid_1 omega" id="downArrow">
+                <a href="#" class="arrow-link"><h1>&#x3009;</h1></a>
+            </div>
+        </div>
 
-<BR /><BR /><BR />
-<a href="http://mahni.jokeswaps.com" target="_top">Mahni's Joke Swap</a><BR />
-<a href="http://tammi.jokeswaps.com" target="_top">Tammi's Joke Swap</a><BR />
-<a href="http://jack.jokeswaps.com" target="_top">Jack's Joke Swap</a><BR />
-<a href="http://jax.jokeswaps.com" target="_top">Jax's Joke Swap</a><BR />
-<a href="http://felix.jokeswaps.com" target="_top">Felix's Joke Swap</a><BR />
+    </section>
+    <!--End of  question section-->
 
+    </div>
+    <!--End of Body wrapper-->
+    <footer id="footerBg">
+        <form id="contactForm" class="container_16">
+
+            <div class="prefix_8 grid_3 alpha label">
+
+                <label for="name" class="grid_3">Name - </label>
+
+                <label for="emial" class="grid_3">E-mail - </label>
+
+                <label for="Message" class="grid_3">Message - </label>
+
+            </div>
+
+
+            <div class="grid_5 omega inputs">
+
+                <input type="text" name="name" class="grid_5" id="contactName" />
+
+                <input type="email" name="email" class="grid_5" id="contactEmail" />
+
+                <textarea name="message" id="contactMessage" cols="30" rows="8"></textarea>
+                <a href="#"><h1 class="prefix_3 grid_2" id="contactSubmit">GO!</h1></a>
+
+            </div>
+
+
+        </form>
 <BR />
 About: When the 7 year old twins Tammi and Mahni created their first web pages, they were quickly dissapointed to discover that only they could add jokes to their personal pages, and not to their sisters page. They needed a 'JokeSpace' or 'JokeTime' - a social media site allowing them to swap jokes, a few tips from dad and they were coding away creating a SQL database and writting up a php script to save and display the jokes. Ofcourse the dinner table discussion that night quickly turned into a discussion of the sites rules and terms and conditions, which will be also written by kids for kids. Visit back regularly to see the site progress as they choose their style for the site and hopefully one day open it for parents to register their kids.
+
+
+<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br /><span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">JokeSwaps</span> by <a xmlns:cc="http://creativecommons.org/ns#" href="https://github.com/karwalski/jokeswaps" property="cc:attributionName" rel="cc:attributionURL">Karwalski</a> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.
+
+    </footer>
+</body>
+
+</html>
+
+
+
+
 
 
 
