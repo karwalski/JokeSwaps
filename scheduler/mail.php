54dbcae1c5d74ef7a5b959474b286630dd205e13
Matt Watt <matt@karwalski.net>
2016-01-30 07:58:46 +1100

    create mail.php

---
+++ scheduler/mail.php
@@ -0,0 +1,131 @@
+<?PHP
+		require_once('class.phpmailer.php');
+		$servername = "localhost";
+		 $username = "root";
+		// $username = "matt";
+		$password = "YmQCl60qMwe2YpUn34k7";
+		$dbname = "jokeswaps";
+		// Create connection
+		$conn = new mysqli($servername, $username, $password, $dbname );
+		// Check connection
+		if ($conn->connect_error) {
+		    die("Connection failed: " . $conn->connect_error);
+		}
+		function ProcessMailQueue(){
+			// This is the mail queue function to be called when new tokens created
+			
+			exec("wget -qO- http://jokeswaps.com/mail.php?process_email_queue=true &> /dev/null &");
+			
+		}
+		if (isset($_GET["process_email_queue"]) && $_GET["process_email_queue"] == 'true')
+		{
+   		 $sql = "SELECT * FROM emailQueue WHERE sent = '0'" ;
+   		 $result = $conn->query($sql);
+		 
+		 if ($result->num_rows > 0) {
+		 	// output data of each row
+		     while($row = $result->fetch_assoc()) {
+				 
+				 // Read email data
+				 $TokenID = $row["TokenID"];
+				 $EmailID = $row["EmailID"];
+					 
+			   		 $sql = "SELECT * FROM tokens WHERE TokenID = '$TokenID'" ;
+			   		 $tokensresult = $conn->query($sql);
+					 
+					 for ($tokenInfo = array (); $tokensrow = $tokensresult->fetch_assoc(); $tokenInfo[] = $tokensrow); 
+					 
+ 				 	$tokenHash = $tokenInfo[0]["hash"];
+				 	$username = $tokenInfo[0]["username"];
+				 	$tokenType = $tokenInfo[0]["type"];
+				 
+		   		 $sql = "SELECT * FROM users WHERE username = '$username'" ;
+		   		 $usersresult = $conn->query($sql);
+				 
+				 for ($userInfo = array (); $usersrow = $usersresult->fetch_assoc(); $userInfo[] = $usersrow); 
+ 
+			 	$email = $userInfo[0]["email"];
+				 
+				 // Send email
+				 if ($tokenType == 'reset')
+					 { mailReset($username, $email, $tokenHash, $EmailID); }
+				 elseif ($tokenType == 'verify')
+					 { mailVerify($username, $email, $tokenHash, $EmailID); }
+				 
+				 
+			 }
+			 
+		 }
+			
+		}
+function mailReset($username, $email, $tokenHash, $EmailID) {
+	global $conn;
+  $sql = "UPDATE emailQueue SET sent = 4, error='sending' WHERE EmailID = '$EmailID' " ;
+  if ($conn->query($sql) === TRUE) {
+	  // Visual feedback if required
+  }
+	
+	
+ $mail             = new PHPMailer(); // defaults to using php "mail()"
+ $mail->IsSendmail(); // telling the class to use SendMail transport
+ $body             = 'Someone has requested a password reset for the JokeSwaps.com account "' . $username . '", which is associated with your email address, if it was not you no action is required and you can ignore this email, the account will remain secure.' . 
+ ' <BR />If you did request a password reset, please confirm by clicking <a href="http://www.jokeswaps.com/parents.php?r=' . $tokenHash . '&username=' . $username . '">here</a>' . 
+ ' or copy and pasting the following link into your browser: ' .
+ 'http://www.jokeswaps.com/parents.php?r=' . $tokenHash . '&username=' . $username . 
+ ' <BR /><BR />From the Friendly JokeSwaps Robot';
+ $mail->AddReplyTo("admin@jokeswaps.com","JokeSwaps");
+ $mail->SetFrom('robot@jokeswaps.com', 'JokeSwaps Robot');
+ $mail->AddAddress($email, "JokeSwaps Parent");
+ $mail->Subject    = "Password reset";
+ $mail->AltBody    = 'Someone has requested a password reset for the JokeSwaps.com account "' . $username . '", which is associated with your email address, if it was not you no action is required and you can ignore this email, the account will remain secure.' . 
+ '\r\nIf you did request a password reset, please confirm by copy and pasting the following link into your browser: ' .
+ 'http://www.jokeswaps.com/parents.php?r=' . $tokenHash . '&username=' . $username . 
+ '\r\n\r\nFrom the Friendly JokeSwaps Robot';
+ $mail->MsgHTML($body);
+if(!$mail->Send()) {
+  $errorMessage = $mail->ErrorInfo;
+  $sql = "UPDATE emailQueue SET sent=3, error='$errorMessage' WHERE EmailID='$EmailID'";
+  $conn->query($sql);
+} else {
+  $sql = "UPDATE emailQueue SET sent=1, error='sent' WHERE EmailID='$EmailID'";
+  $conn->query($sql);
+}
+	
+}
+function mailVerify($username, $email, $tokenHash, $EmailID)
+	
+{
+	global $conn;
+  $sql = "UPDATE emailQueue SET sent = 4, error='sending' WHERE EmailID = '$EmailID' " ;
+  if ($conn->query($sql) === TRUE) {
+	  // Visual feedback if required
+  }
+	
+	$mail             = new PHPMailer(); // defaults to using php "mail()"
+	$mail->IsSendmail(); // telling the class to use SendMail transport
+	$body             = 'Someone has signed up to JokeSwaps.com using your email address, if it was not you no action is required and you can ignore this email.' . 
+	' <BR /><BR />If you did sign up to JokeSwaps.com, please confirm your email address by clicking <a href="http://www.jokeswaps.com/parents.php?v=' . $tokenHash . '&username=' . $username . '">here</a>' . 
+	' or copy and pasting the following link into your browser: ' .
+	'http://www.jokeswaps.com/parents.php?v=' . $tokenHash . '&username=' . $username . 
+	' <BR /><BR />From the Friendly JokeSwaps Robot';
+	$mail->AddReplyTo("admin@jokeswaps.com","JokeSwaps");
+	$mail->SetFrom('robot@jokeswaps.com', 'JokeSwaps Robot');
+	$mail->AddAddress($email, "JokeSwaps Parent");
+	$mail->Subject    = "Please confirm your email address";
+	$mail->AltBody    = 'Someone has signed up to JokeSwaps.com using your email address, if it was not you no action is required and you can ignore this email.' . 
+	'\r\nIf you did sign up to JokeSwaps.com, please confirm your email address by copy and pasting the following link into your browser: ' .
+	'http://www.jokeswaps.com/parents.php?v=' . $tokenHash . '&username=' . $username . 
+	'\r\n\r\nFrom the Friendly JokeSwaps Robot';
+	$mail->MsgHTML($body);
+	if(!$mail->Send()) {
+	  $errorMessage = $mail->ErrorInfo;
+	  $sql = "UPDATE emailQueue SET sent=3, error='$errorMessage' WHERE EmailID='$EmailID'";
+	  $conn->query($sql);
+	} else {
+	  $sql = "UPDATE emailQueue SET sent=1, error='sent' WHERE EmailID='$EmailID'";
+	  $conn->query($sql);
+	}
+	
+}
+$conn->close();
+?>
